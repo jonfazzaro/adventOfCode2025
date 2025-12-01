cca4571f96cb83751946217fe93dc8ce447c1feb
jonfazzaro <jon@fazzaro.com>
2025-12-01 13:24:49 -0500

    wip

--- src/day/1/combination.spec.ts
+++ src/day/1/combination.spec.ts
@@ -5,7 +5,30 @@ describe('The combination', () => {
 
   it('is designed in this playground', () => {
 
-    const input = Input.create("");
+    const input = Input.create("L68\n" +
+      "L30\n" +
+      "R48\n" +
+      "L5\n" +
+      "R60\n" +
+      "L55\n" +
+      "L1\n" +
+      "L99\n" +
+      "R14\n" +
+      "L82");
+
+    const combo = Combination.create()
+    combo.find(input.lines)
+
 
   });
 });
+
+class Combination {
+  static create() {
+    return new Combination();
+  }
+
+  find(lines: string[]) {
+
+  }
+}

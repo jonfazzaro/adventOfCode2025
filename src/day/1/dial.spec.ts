af6039624c7eff5bbf74a7bcb9c32d1dac5f8339
jonfazzaro <jon@fazzaro.com>
2025-12-01 11:41:57 -0500

    wip

--- src/day/1/dial.spec.ts
+++ src/day/1/dial.spec.ts
@@ -8,6 +8,17 @@ describe('The dial', () => {
   it.todo('starts at position 50');
   describe('given a R3', () => {
     it.todo('has position 53');
+    describe('then L10', () => {
+      it.todo('has position 43');
+    });
+  });
+
+  describe('given L50', () => {
+    it.todo('has position 0');
+
+    describe('then L23', () => {
+      it.todo('has position 67');
+    });
   });
 
 });

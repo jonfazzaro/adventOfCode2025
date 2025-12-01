fba4bede7c70e6302c788db908f14ac3d58121b0
jonfazzaro <jon@fazzaro.com>
2025-12-01 13:22:28 -0500

    wip

---
+++ src/day/1/combination.spec.ts
@@ -0,0 +1,11 @@
+import {Input} from "../../input.ts";
+
+describe('The combination', () => {
+
+
+  it('is designed in this playground', () => {
+
+    const input = Input.create("");
+
+  });
+});

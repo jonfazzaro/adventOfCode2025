36cdee00cf0027ee6ec614ca322730b67fa9fd26
jonfazzaro <jon@fazzaro.com>
2025-12-02 08:49:11 -0500

    wip

--- src/day/2/id-validator.spec.ts
+++ src/day/2/id-validator.spec.ts
@@ -9,7 +9,6 @@ describe('The ID validator', () => {
 
   describe('given a range with no repeating digits', () => {
     it('returns no invalid IDs', () => {
-      const validator = IdValidator.create();
       const invalidIDs = validator.invalidIDs(11, 22);
       expect(invalidIDs).toEqual([])
     });
@@ -17,7 +16,6 @@ describe('The ID validator', () => {
   });
 
   it.skip('solves the example', () => {
-    const validator = IdValidator.create();
     const input = Input.create("11-22,95-115,998-1012,1188511880-1188511890,222220-222224," +
       "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
       "824824821-824824827,2121212118-2121212124")

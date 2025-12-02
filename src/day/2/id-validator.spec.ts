503e7819217740f27e4b4f2943fdb9e489d746c0
jonfazzaro <jon@fazzaro.com>
2025-12-02 08:33:57 -0500

    wip

--- src/day/2/id-validator.spec.ts
+++ src/day/2/id-validator.spec.ts
@@ -2,7 +2,7 @@ import {IdValidator} from "./id-validator.ts";
 
 describe('The ID validator', () => {
   it('exists', () => {
-    expect(new IdValidator()).toBeInstanceOf(IdValidator)
+    expect(IdValidator.create()).toBeInstanceOf(IdValidator)
   });
 
 });

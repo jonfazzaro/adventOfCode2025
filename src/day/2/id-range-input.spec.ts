5949322854aefab3801623abaf0d44442aebb378
jonfazzaro <jon@fazzaro.com>
2025-12-03 08:29:49 -0500

    wip

--- src/day/2/id-range-input.spec.ts
+++ src/day/2/id-range-input.spec.ts
@@ -6,4 +6,12 @@ describe('The ID range input', () => {
     expect(IdRangeInput.create()).toBeInstanceOf(Input);
   });
 
+  it('parses ranges', () => {
+    const subject = Input.create("11-22,95-115,998-1012", ',');
+    expect(subject.ranges()).toEqual(expect.arrayContaining([
+      [11,22],
+      [95,115],
+      [998,1012]
+    ]))
+  });
 });

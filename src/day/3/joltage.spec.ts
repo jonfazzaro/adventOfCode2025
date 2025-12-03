90fa07aa12d5920ac6559c3d5d84be1aaaec4213
jonfazzaro <jon@fazzaro.com>
2025-12-03 10:04:51 -0500

    wip

--- src/day/3/joltage.spec.ts
+++ src/day/3/joltage.spec.ts
@@ -31,6 +31,21 @@ describe('The joltage meter', () => {
     });
   });
 
+  describe('when taking 12 batteries', () => {
+
+    describe('given a bank of batteries', () => {
+      it.each([
+        ["0145671", 71],
+        ["987654321111111", 98],
+        ["811111111111119", 89]
+      ])('measures its capacity', (input, expected) => {
+        const bankInput = BatteryInput.create(input)
+        const subject = Joltage.create(bankInput.banks()[0]);
+        expect(subject.capacity()).toEqual(expected)
+      });
+    });
+  });
+
   function sum(input: number[]) {
     return input.reduce((total, num) => total + num, 0);
   }

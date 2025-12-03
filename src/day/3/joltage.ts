1f6984235c2f2a3849c1ba9909772f02bc89d794
jonfazzaro <jon@fazzaro.com>
2025-12-03 09:08:34 -0500

    wip

--- src/day/3/joltage.ts
+++ src/day/3/joltage.ts
@@ -4,8 +4,12 @@ export class Joltage {
   }
 
   capacity(bank: number[]) {
-    return parseInt(bank.sort((a, b) => b - a)
+    return parseInt(this.sortDescending(bank)
       .slice(0, 2)
       .join(''))
+  }
+
+  private sortDescending(bank: number[]) {
+    return bank.sort((a, b) => b - a);
   }
 }

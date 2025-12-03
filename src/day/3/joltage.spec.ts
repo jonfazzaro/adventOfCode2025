0119ca14300a6216acef5279a7ef43ef19fa6c37
jonfazzaro <jon@fazzaro.com>
2025-12-03 09:03:01 -0500

    wip

--- src/day/3/joltage.spec.ts
+++ src/day/3/joltage.spec.ts
@@ -8,10 +8,9 @@ describe('The joltage meter', () => {
   describe('given a bank of batteries', () => {
     it.each([
       [[0,1,4,5,6,7,1], 76],
-    ])('measures its capacity', () => {
-      const bank = [0,1,4,5,6,7,1];
+    ])('measures its capacity', (bank, expected) => {
       const subject = Joltage.create();
-      expect(subject.capacity(bank)).toEqual(76)
+      expect(subject.capacity(bank)).toEqual(expected)
     });
 
   });

27c2a618fc819f38591ff47302ec53c561004df8
jonfazzaro <jon@fazzaro.com>
2025-12-03 08:30:54 -0500

    wip

--- src/day/2/id-range-input.spec.ts
+++ src/day/2/id-range-input.spec.ts
@@ -1,13 +1,8 @@
-import {Input} from "../../input.ts";
 import {IdRangeInput} from "./id-range.input.ts";
 
 describe('The ID range input', () => {
-  it('should exist', () => {
-    expect(IdRangeInput.create()).toBeInstanceOf(Input);
-  });
-
   it('parses ranges', () => {
-    const subject = Input.create("11-22,95-115,998-1012", ',');
+    const subject = IdRangeInput.create("11-22,95-115,998-1012", ',');
     expect(subject.ranges()).toEqual(expect.arrayContaining([
       [11,22],
       [95,115],
--- src/day/2/id-range.input.ts
+++ src/day/2/id-range.input.ts
@@ -1,12 +1,12 @@
 import {Input} from "../../input.ts";
 
 export class IdRangeInput extends Input {
-  constructor() {
-    super("", ',');
+  constructor(input: string) {
+    super(input, ',');
   }
 
-  static create() {
-    return new IdRangeInput()
+  static create(input: string) {
+    return new IdRangeInput(input)
   }
 
 }

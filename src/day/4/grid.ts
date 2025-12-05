4cca9285705925c4c1d4bf7f4a6d6bc23bbc7825
jonfazzaro <jon@fazzaro.com>
2025-12-05 10:54:09 -0500

    wip

--- src/day/4/grid.ts
+++ src/day/4/grid.ts
@@ -1,12 +1,12 @@
 export class Grid {
-  private height: number;
+  public height: number;
+  public width: number;
+
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
-
   }
 
-  width: number;
   static create(width: number, height: number) {
     return new Grid(width, height);
   }

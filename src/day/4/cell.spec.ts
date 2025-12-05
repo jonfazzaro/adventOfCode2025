d763b802b9c4c841d60880ace64599e610dbcb11
jonfazzaro <jon@fazzaro.com>
2025-12-05 10:49:16 -0500

    wip

--- src/day/4/cell.spec.ts
+++ src/day/4/cell.spec.ts
@@ -1,11 +1,7 @@
 import {Cell} from "./cell.ts";
 
 describe('The grid cell', () => {
-  it('should exist', () => {
-    expect(Cell.create()).toBeInstanceOf(Cell);
-  });
-
-  it('has a neighbor to the north', () => {
+  it('has neighbors', () => {
     const north = Cell.create();
     expect(Cell.create([north]).neighbors).toEqual([north])
   });

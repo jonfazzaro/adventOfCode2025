bca9d0547f48bf20822f1f68e584d428abbd831a
jonfazzaro <jon@fazzaro.com>
2025-12-05 10:54:29 -0500

    wip

--- src/day/4/grid.spec.ts
+++ src/day/4/grid.spec.ts
@@ -1,10 +1,6 @@
 import {Grid} from "./grid.ts";
 
 describe('The grid', () => {
-  it('exists', () => {
-    expect(Grid.create()).toBeInstanceOf(Grid);
-  });
-
   it('has a width and height', () => {
     const grid = Grid.create(3, 5);
     expect(grid.width).toEqual(3);

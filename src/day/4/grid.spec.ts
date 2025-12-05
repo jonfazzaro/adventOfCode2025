e2163a06f188d912904773ea770d3164ba8fe990
jonfazzaro <jon@fazzaro.com>
2025-12-05 13:51:08 -0500

    wip

--- src/day/4/grid.spec.ts
+++ src/day/4/grid.spec.ts
@@ -1,5 +1,6 @@
 import {Grid} from "./grid.ts";
 import {Point} from "./point.ts";
+import {beforeEach} from "vitest";
 
 describe('The grid', () => {
   it('has a width and height', () => {
@@ -28,9 +29,13 @@ describe('The grid', () => {
 
 
     describe('on the west edge', () => {
+      let grid: Grid;
+      beforeEach(() => {
+        grid = Grid.create(5, 5);
+      });
+
       describe('at the north edge', () => {
         it('has a neighbor to the east', () => {
-          const grid = Grid.create(5, 5);
           expect(grid.eastOf(Point.create(0, 0))).toEqual({ x: 1, y: 0, value: '.' })
         });
 
@@ -38,6 +43,7 @@ describe('The grid', () => {
           const grid = Grid.create(5, 5);
           expect(grid.westOf(Point.create(0, 0))).toBeUndefined()
         });
+
 
       });
     });

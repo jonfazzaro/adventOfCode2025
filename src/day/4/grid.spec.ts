35c2156d137c9f6612887600f81941ccb8140309
jonfazzaro <jon@fazzaro.com>
2025-12-06 11:57:20 -0500

    wip

--- src/day/4/grid.spec.ts
+++ src/day/4/grid.spec.ts
@@ -1,6 +1,4 @@
 import {Grid} from "./grid.ts";
-import {Point} from "./point.ts";
-import {beforeEach} from "vitest";
 
 describe('The grid', () => {
   it('has a width and height', () => {
@@ -9,45 +7,5 @@ describe('The grid', () => {
     expect(grid.height).toEqual(5);
   });
 
-  it.each([
-    [[2, 2], [
-      '..',
-      '..',
-    ]],
-    [[3, 4], [
-      '...',
-      '...',
-      '...',
-      '...',
-    ]]
-  ])('has cells', ([width, height]: number[], expected: string[]) => {
-    const grid = Grid.create(width, height);
-    expect(grid.cells).toEqual(expected)
-  });
-
-  describe('given a cell', () => {
-
-
-    describe('on the west edge', () => {
-      let grid: Grid;
-
-      beforeEach(() => {
-        grid = Grid.create(5, 5);
-      });
-
-      describe('at the north edge', () => {
-        it('has a neighbor to the east', () => {
-          expect(grid.eastOf(Point.create(0, 0))).toEqual({ x: 1, y: 0, value: '.' })
-        });
-
-        it('has no neighbor to the west', () => {
-          expect(grid.westOf(Point.create(0, 0))).toBeUndefined()
-        });
-
-
-      });
-    });
-
-  });
 
 });

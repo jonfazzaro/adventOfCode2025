f70c56fd202522058a46db9e8fd070165ee08cbd
jonfazzaro <jon@fazzaro.com>
2025-12-06 12:17:34 -0500

    wip

--- src/day/4/grid.ts
+++ src/day/4/grid.ts
@@ -15,10 +15,8 @@ export class Grid {
 
   at(x: number, y: number) {
     if (this.isOutOfBounds(x, y)) return "";
-
     return (this.rows)[y][x]
   }
-
   private isOutOfBounds = (x: number, y: number) =>
     this.isOutOfBoundsHorizontally(x) || this.isOutOfBoundsVertically(y);
   private isOutOfBoundsVertically = (y: number) =>

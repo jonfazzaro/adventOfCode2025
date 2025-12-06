bcc799c06e66ae0a31655e131b83ebddc7237796
jonfazzaro <jon@fazzaro.com>
2025-12-06 11:57:55 -0500

    wip

--- src/day/4/grid.ts
+++ src/day/4/grid.ts
@@ -5,21 +5,13 @@ const defaultValue = '.';
 export class Grid {
   public height: number;
   public width: number;
-  public cells: Array<string> = [];
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
-    this.cells = Array(height).fill(this.newRow(width));
   }
 
   static create(width: number, height: number) {
     return new Grid(width, height);
   }
-
-  private newRow(width: number) {
-    return Array(width).fill(defaultValue).join('');
-  }
-  
-
 }

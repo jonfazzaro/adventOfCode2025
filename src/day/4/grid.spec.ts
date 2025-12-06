d79e7824c833f1810cad75c8d7ffba5cd38c4a65
jonfazzaro <jon@fazzaro.com>
2025-12-06 12:11:40 -0500

    wip

--- src/day/4/grid.spec.ts
+++ src/day/4/grid.spec.ts
@@ -13,6 +13,7 @@ describe('The grid', () => {
     [3, 0, "@"],
     [9, 5, "@"],
     [-1, 5, ""],
+    [10, 7, ""],
   ])('accesses the value at a location', (x: number, y: number, expected: string) => {
     const grid = Grid.create(exampleInput.values);
     expect(grid.at(x, y)).toEqual(expected);

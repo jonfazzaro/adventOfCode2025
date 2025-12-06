46aa79de9e70e2fb219c5268cfbd932207eb19ac
jonfazzaro <jon@fazzaro.com>
2025-12-06 12:01:21 -0500

    wip

--- src/day/4/grid.spec.ts
+++ src/day/4/grid.spec.ts
@@ -3,20 +3,20 @@ import {Input} from "../../input.ts";
 
 describe('The grid', () => {
   it('has a width and height', () => {
-    const exampleInput = Input.create("..@@.@@@@.\n" +
-      "@@@.@.@.@@\n" +
-      "@@@@@.@.@@\n" +
-      "@.@@@@..@.\n" +
-      "@@.@@@@.@@\n" +
-      ".@@@@@@@.@\n" +
-      ".@.@.@.@@@\n" +
-      "@.@@@.@@@@\n" +
-      ".@@@@@@@@.\n" +
-      "@.@.@@@.@.")
     const grid = Grid.create(exampleInput.values);
     expect(grid.width).toEqual(10);
     expect(grid.height).toEqual(10);
   });
-
-
 });
+
+
+const exampleInput = Input.create("..@@.@@@@.\n" +
+  "@@@.@.@.@@\n" +
+  "@@@@@.@.@@\n" +
+  "@.@@@@..@.\n" +
+  "@@.@@@@.@@\n" +
+  ".@@@@@@@.@\n" +
+  ".@.@.@.@@@\n" +
+  "@.@@@.@@@@\n" +
+  ".@@@@@@@@.\n" +
+  "@.@.@@@.@.")

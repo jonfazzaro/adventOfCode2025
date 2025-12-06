856e1b5beff80e8b72289ead1f3adbbc9386e6be
jonfazzaro <jon@fazzaro.com>
2025-12-06 12:12:51 -0500

    wip

--- src/day/4/grid.spec.ts
+++ src/day/4/grid.spec.ts
@@ -2,11 +2,6 @@ import {Grid} from "./grid.ts";
 import {Input} from "../../input.ts";
 
 describe('The grid', () => {
-  it('has a width and height', () => {
-    const grid = Grid.create(exampleInput.values);
-    expect(grid.width).toEqual(10);
-    expect(grid.height).toEqual(10);
-  });
 
   it.each([
     [0, 0, "."],

8b97960f72a3cf43a3238512d0db60589a1a1442
jonfazzaro <jon@fazzaro.com>
2025-12-11 12:37:06 -0500

    wip

--- src/day/4/grid.spec.ts
+++ src/day/4/grid.spec.ts
@@ -55,7 +55,7 @@ describe('The grid', () => {
   it.each([
     [[{x:-1, y:0}], ""],
     [[{x:1, y:0}], "."],
-    [[{x:2, y:2}], "."],
+    [[{x:2, y:2}, {x:3, y:4}], "."],
   ])('removes multiple cells', (points: Point[], expected: string) => {
     expect(grid.withManyRemoved(points).at(points[0].x, points[0].y)).toEqual(expected)
   });
